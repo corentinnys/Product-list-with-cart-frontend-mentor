86710b521138a84f1fc5543a8661e0aa0dfe6bad
Corentin <corentinys@gmail.com>
2026-03-22 16:53:18 +0100

    jsx pour les cards

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,26 +1,70 @@
 import React, { Component } from "react";
 import "../cards.css";
+
 class Cards extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            quantity: 0,
+        };
     }
 
+    increment = () => {
+        this.setState(
+            (prevState) => ({ quantity: prevState.quantity + 1 }),
+            () => {
+                this.props.onAddToCart({
+                    name: this.props.name,
+                    price: this.props.price,
+                    quantity: this.state.quantity
+                });
+            }
+        );
+    };
+
+    decrement = () => {
+        if (this.state.quantity > 0) {
+            this.setState(
+                (prevState) => ({ quantity: prevState.quantity - 1 }),
+                () => {
+                    // ✅ Appelé APRÈS que l'état est mis à jour
+                    this.props.onAddToCart({
+                        name: this.props.name,
+                        price: this.props.price,
+                        quantity: this.state.quantity
+                    });
+                }
+            );
+        }
+    };
+
     render() {
-        const { name, price, image,category } = this.props;
+        const { name, price, image, category } = this.props;
+        const { quantity } = this.state;
+
         return (
             <div className="card">
-                <a href="#" className="btn-add">Add to Cart</a>
-                <figure>
-                    <img src={image.desktop} alt=""/>
+                {quantity === 0 ? (
+                    <button className="btn-add" onClick={this.increment}>
+                        Add to Cart
+                    </button>
+                ) : (
+                    <div className="quantity-box">
+                        <button onClick={this.decrement}>➖</button>
+                        <span>{quantity}</span>
+                        <button onClick={this.increment}>➕</button>
+                    </div>
+                )}
 
+                <figure>
+                    <img src={image.desktop} alt={name} />
                     <figcaption>{category}</figcaption>
                 </figure>
 
                 <h3>{name}</h3>
                 <p>${price}</p>
-
             </div>
-
         );
     }
 }
